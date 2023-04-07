cb48083ef1197eeaf04eea496c159602e0d87396
clarence101701 <clarence.coronell@gmail.com>
2023-04-07 11:43:24 +0800

    finish

--- script/functions.js
+++ script/functions.js
@@ -2,12 +2,39 @@ let bgIdx = 0;
 let isUpped = false;
 let doAnim = true;
 let isChangeColor = false;
+let isH2Change = false;
 const body = document.getElementsByTagName('body')[0];
 const imageContainer = document.querySelector(".image-container");
 const aboutText = document.querySelector(".text-container");
 const highlight = document.querySelectorAll(".highlight");
 const p = document.querySelector('.text-container p');
 const modalTrigger = document.getElementById('modal-trigger');
+const h2 = document.querySelectorAll('h2');
+const actItem = document.querySelectorAll('.act-item');
+
+setInterval(()=>{
+    if(isH2Change){
+
+        for(i = 0; i <h2.length; i++){
+            h2[i].style.color = 'aquamarine';
+            actItem[i].style.border = '2px solid skyblue'
+            isH2Change = false;
+        }
+
+        
+    }else{
+
+        for(i = 0; i <h2.length; i++){
+            h2[i].style.color = 'pink';
+            actItem[i].style.border = '2px solid pink'
+            isH2Change = true;
+        }
+        
+    }
+},400)
+
+
+
 
 if(window.innerWidth <= 1100){
     generateModal('Note', 'Ma\'am Jho, my portfolio website is not mobile responsive and is best viewed in PC. Sorry for the inconvenience and thank you po!');
